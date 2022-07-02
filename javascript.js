71f3d898877b7a0e7bf84c14892c3851fa12e5ad
trongnghia <maivantrongnghia64@gmail.com>
2022-07-02 15:45:25 +0700

    control flow basics

--- javascript.js
+++ javascript.js
@@ -1,11 +1,65 @@
-// obj
-let a = {
-    name: "nghia",
-    age: 22,
-    a: function(){
-        console.log("hello world")
-        return
-    }
+// for loops 
+let arr = ['nghia', 'nghia1', 'ngocanh','ngocanh1'];
 
-};
-console.log(typeof a.name)
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i].length % 2 == 0 )  console.log(arr[i]);
+   
+// };  
+
+
+// while loops
+// let i = 0;
+// while(i < arr.length)
+// {
+//     console.log(arr[i])
+//     i++;
+// }
+
+
+// break
+
+// for(let i = 0; i < arr.length; i++){
+//     console.log('i =', i)
+//     if(arr[i] === 'nghia1' ) {
+//         console.log(arr[i]);
+//         break;
+//     }
+  
+// };  
+
+
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] === 'nghia1' ) {
+//         console.log(arr[i]);
+//     }
+//     console.log('i =', i)
+// };  
+
+
+
+// switch case
+let day = "";
+switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  };
+
+  console.log(day)
